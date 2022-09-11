0e9b6c43d701193dbf104f32a4b1cd84a60d9c27
Jiwoo Hong <93971542+ghdwldn0914@users.noreply.github.com>
2022-09-12 05:17:57 +0900

    Update 1026.cpp

--- 09월 02일 - 정렬/1026.cpp
+++ 09월 02일 - 정렬/1026.cpp
@@ -6,28 +6,26 @@ using namespace std;
 
 
 int main() {
-	int n; //길이
+	int n; //湲몄씠
 
 	cin >> n;
 
 	vector<int> A(n);
 	vector<int> B(n);
-	vector<int> Copy(n); //B의 배열을 복사한 배열을 재배열하여 사용
 
 	for (int i = 0; i < n; i++) {
 		cin >> A[i];
 	}
 	for (int i = 0; i < n; i++) {
 		cin >> B[i];
-		Copy[i] = B[i];
 	}
 
 	sort(A.begin(), A.end());
-	sort(Copy.begin(), Copy.end(), greater<>());
+	sort(B.begin(), B.end(), greater<>());
 
 	int sum = 0;
 	for (int i = 0; i < n; i++) {
-		sum += A[i] * Copy[i];
+		sum += A[i] * B[i];
 	}
 
 	cout << sum;
